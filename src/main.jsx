79a05af3458e1cc85e535aa9b6c6ae98b3cfa37b
hafizhsul <hafizhbachtiyar123@gmail.com>
2026-09-18 19:18:00 +0700

    feat(theme): editorial light + obsidian dark tokens, Jakarta/Inter fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@fontsource-variable/sora";
-import "@fontsource/ibm-plex-mono";
+import "@fontsource/plus-jakarta-sans/600.css";
+import "@fontsource/plus-jakarta-sans/700.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
 import "./index.css";
 import App from "./App.jsx";
 import { I18nProvider } from "./i18n";
+import { ThemeProvider } from "./theme";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <I18nProvider>
-      <App />
-    </I18nProvider>
+    <ThemeProvider>
+      <I18nProvider>
+        <App />
+      </I18nProvider>
+    </ThemeProvider>
   </StrictMode>,
 );
